a9027219f034f2970c980fd86bdaa2bc4a48361a
Henko Rabie <henkorabie99@gmail.com>
2021-01-11 07:25:14 +0200

    Adding Initial web api

---
+++ WordStack.Api/Controllers/WordController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordStack.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordController : ControllerBase
+    {
+        private readonly ILogger<WordController> _logger;
+
+        public WordController(ILogger<WordController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
